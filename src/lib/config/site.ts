f6474663e8abf5566b20bbdefea2ee0958487dfe
freesnowboarding <159198967+freesnowboarding@users.noreply.github.com>
2024-02-07 18:22:32 +0100

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -4,7 +4,7 @@ export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'localhost:5173',
   title: 'freesnowboarding.xyz',
-  subtitle: 'B Happy',
+  subtitle: 'Happy',
   lang: 'en-US',
   description: 'Powered by SvelteKit/Urara',
   author: {  
