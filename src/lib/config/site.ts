2ff16d8eda3df0680e10f2e91c34f845b96b1565
freesnowboarding <159198967+freesnowboarding@users.noreply.github.com>
2024-02-07 19:50:35 +0100

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -10,6 +10,6 @@ export const site: SiteConfig = {
   author: {  
   avatar: '/assets/gshit.png',
     name: 'Drink Water',
-  status: '<a href=https://www.wedrinkwater.com>🚰</a>',
+  status: '<a href=https://www.wedrinkwater.com/>🚰</a>',
     bio: '<a href=https://www.waldhaus-bier.de/>🌊</a> '}
 }
