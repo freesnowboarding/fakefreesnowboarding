5910cacfb11ffe1536267c6cfc5043e217a7d3e8
freesnowboarding <159198967+freesnowboarding@users.noreply.github.com>
2024-02-07 18:27:37 +0100

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'https://maerle7.work',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'maerle7.work',
   title: 'freesnowboarding.xyz',
   subtitle: 'Happy',
   lang: 'en-US',
